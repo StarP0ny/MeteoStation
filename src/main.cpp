2d8b485e33fa1a52f3906f0b88d64bff17d28c0f
StarPony <ivan.fedorchuk.42@gmail.com>
2022-12-01 15:40:41 +0300

    cleaning

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,9 @@ TM1637Display display(disp_clk, disp_data);
 
 void setup() 
 {
-  //Serial.begin(9600);                          // Задаем скорость передачи данных
-  //Serial.println("DHT22 TEST!");               // Выводим текст
   dht.begin();
   display.setBrightness(0x0f);
-  //data[0] = display.encodeDigit(0);
-  //data[1] = display.encodeDigit(1);
-  //data[2] = display.encodeDigit(2);
-  //data[3] = display.encodeDigit(3);
   display.setSegments(data, 4, 0);
-  //disp.showNumberDec(1, true);
 }
  
 void loop() 
@@ -25,6 +18,4 @@ void loop()
 
   display.showNumberDec(t, true, 2, 0);
   display.showNumberDec(h, true, 2, 2);
-  //display.showDots(1, 4);
-
 }
